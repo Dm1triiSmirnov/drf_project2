253c0b04b8f737023292ba986e68fe544d757559
Dm1triiSmirnov <smirnov0719@gmail.com>
2023-03-29 13:54:33 +0400

    modifiy wallet/serializers.py, add limits to create not more then 5 wallets and bonus for new wallets

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -16,3 +16,23 @@ class WalletSerializer(serializers.ModelSerializer):
             "created_on",
             "modified_on",
         )
+
+    def create(self, validated_data):
+        """
+        1)  Check how many wallets the user has.
+            If the user has 5 or more wallets, then raise Exception
+
+        2)  Add default bonus from bank for each new created wallet:
+            if wallet currency USD or EUR - balance=3.00,
+            if RUB - balance=100.00
+        """
+
+        print(validated_data)
+        if Wallet.objects.filter(user__exact=validated_data.get("user")).count() >= 5:
+            raise Exception("User can't create more than 5 wallets")
+        if validated_data["currency"] in ["USD", "EUR"]:
+            validated_data["balance"] += 3
+        elif validated_data["currency"] == "RUB":
+            validated_data["balance"] += 100
+
+        return Wallet.objects.create(**validated_data)
--- wallet/views.py
+++ wallet/views.py
@@ -3,23 +3,17 @@ from rest_framework import mixins, viewsets
 from wallet.models import Wallet
 from wallet.serializers import WalletSerializer
 
-# class WalletViewSet(viewsets.ModelViewSet):
-#     queryset = Wallet.objects.all()
-#     serializer_class = WalletSerializer
 
-
-class WalletsListCreateViewSet(
-    mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
-):
+class WalletsListCreateViewSet(mixins.CreateModelMixin,
+                               mixins.ListModelMixin,
+                               viewsets.GenericViewSet):
     queryset = Wallet.objects.all()
     serializer_class = WalletSerializer
 
 
-class WalletsRetrieveDestroyViewSet(
-    mixins.RetrieveModelMixin,
-    mixins.DestroyModelMixin,
-    viewsets.GenericViewSet,  # noqa E501
-):
+class WalletsRetrieveDestroyViewSet(mixins.RetrieveModelMixin,
+                                    mixins.DestroyModelMixin,
+                                    viewsets.GenericViewSet,):
     queryset = Wallet.objects.all()
     serializer_class = WalletSerializer
     lookup_field = "name"
