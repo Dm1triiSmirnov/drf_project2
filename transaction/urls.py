f87b8d9527ae4b3a645ac67b121485847872d624
Dm1triiSmirnov <smirnov0719@gmail.com>
2023-04-04 12:03:29 +0400

    modify transaction/urls.py & transaction/views.py

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 
-from transaction.views import TransactionListCreateViewSet, TransactionRetrieveDestroyViewSet
-
+from transaction.views import (TransactionListCreateViewSet,
+                               TransactionListViewSet,
+                               TransactionRetrieveDestroyViewSet)
 
 urlpatterns = [
     path("", TransactionListCreateViewSet.as_view({"get": "list",
                                                    "post": "create"})),
-    path("<int:pk>/",
-         TransactionRetrieveDestroyViewSet.as_view({"get": "retrieve",
-                                                   "delete": "destroy"})),
+    path(
+        "<int:pk>/",
+        TransactionRetrieveDestroyViewSet.as_view(
+            {"get": "retrieve", "delete": "destroy"}
+        ),
+    ),
+    path(
+        "<str:sender>/<str:receiver>/",
+        TransactionListViewSet.as_view({"get": "list"})
+    ),
 ]
--- transaction/views.py
+++ transaction/views.py
@@ -1,19 +1,26 @@
-from rest_framework import viewsets, mixins
-
+from rest_framework import mixins, viewsets
 
 from transaction.models import Transaction
 from transaction.serializers import TransactionSerializer
 
 
-class TransactionListCreateViewSet(mixins.CreateModelMixin,
-                                   mixins.ListModelMixin,
-                                   viewsets.GenericViewSet):
+class TransactionListCreateViewSet(
+    mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
 
-class TransactionRetrieveDestroyViewSet(mixins.RetrieveModelMixin,
-                                        mixins.DestroyModelMixin,
-                                        viewsets.GenericViewSet,):
+class TransactionRetrieveDestroyViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+
+class TransactionListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    lookup_fields = ["sender", "receiver"]
