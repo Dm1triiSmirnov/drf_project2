4709a79dad60d716d70ddbe1d135885eaf1d52e2
Dm1triiSmirnov <smirnov0719@gmail.com>
2023-04-04 10:05:33 +0400

    add check for sufficient amount of sender balance

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,5 +1,4 @@
 from _decimal import Decimal
-
 from rest_framework import serializers
 
 from transaction.models import Transaction
@@ -25,35 +24,40 @@ class TransactionSerializer(serializers.ModelSerializer):
         Transactions are available only for wallets with the same currency
         """
 
-        sender_currency = data['sender'].currency
-        receiver_currency = data['receiver'].currency
+        sender_currency = data["sender"].currency
+        receiver_currency = data["receiver"].currency
         if sender_currency != receiver_currency:
-            raise serializers.ValidationError('Sender and receiver wallets must have the same currency.')
+            raise serializers.ValidationError(
+                "Sender and receiver wallets must have the same currency."
+            )
         return data
 
     def create(self, validated_data):
         """
         Calculate the commission based on whether
         the sender and receiver wallets belong to the same user.
-        When user sends money from his wallet to his another wallet - no commission,
-        and when he sends to wallet, related to another user - commission=10%
+        When user sends money from his wallet to his
+        another wallet - no commission, and when he sends
+        to wallet, related to another user - commission=10%
         """
 
         transfer_amount = validated_data["transfer_amount"]
-
-        sender = self.validated_data['sender']
-        receiver = self.validated_data['receiver']
+        sender = self.validated_data["sender"]
+        receiver = self.validated_data["receiver"]
 
         if sender.user == receiver.user:
             commission = 0
         else:
-            commission = transfer_amount * Decimal('0.1')
+            commission = transfer_amount * Decimal("0.1")
+
+        if sender.balance < transfer_amount + commission:
+            raise ValueError("Insufficient balance.")
 
         sender.balance -= transfer_amount + commission
         sender.save()
         receiver.balance += transfer_amount
         receiver.save()
 
-        validated_data['commission'] = commission
+        validated_data["commission"] = commission
 
         return Transaction.objects.create(**validated_data)
