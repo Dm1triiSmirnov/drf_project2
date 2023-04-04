2ae1d249ef31c2004b965ff735c1f1b3bf0e2f20
Dm1triiSmirnov <smirnov0719@gmail.com>
2023-04-04 10:55:29 +0400

    fix transaction/models.py add editable=False to some fields

--- transaction/models.py
+++ transaction/models.py
@@ -16,7 +16,13 @@ class Transaction(models.Model):
         related_name="receiver",
         to_field="name",  # noqa E501
     )
-    transfer_amount = models.DecimalField(max_digits=12, decimal_places=2)
-    commission = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    status = models.CharField(choices=STATUS, max_length=100, default='PAID')
-    timestamp = models.DateTimeField(auto_now_add=True)
+    transfer_amount = models.DecimalField(
+        max_digits=12, decimal_places=2, default=0
+    )  # noqa E501
+    commission = models.DecimalField(
+        max_digits=12, decimal_places=2, default=0, editable=False
+    )
+    status = models.CharField(
+        choices=STATUS, max_length=100, default="PAID", editable=False
+    )
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
