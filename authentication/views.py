3745eb7e6cf03620734925460f539da82d5a8141
Dm1triiSmirnov <smirnov0719@gmail.com>
2023-04-05 17:32:32 +0400

    fix transaction/views.py. Add retrive list of transactions for current user and for given wallet

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views
 from rest_framework.response import Response
 
 from .serializers import RegisterSerializer, UserSerializer
--- transaction/views.py
+++ transaction/views.py
@@ -1,4 +1,4 @@
-from rest_framework import mixins, viewsets, permissions, status
+from rest_framework import mixins, viewsets, status
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 from django.db.models import Q
@@ -13,6 +13,18 @@ class TransactionListCreateViewSet(
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+    def list(self, request, *args, **kwargs) -> Response:
+        """Get all transactions for current user"""
+
+        queryset = Transaction.objects.filter(
+            Q(sender__user=request.user) | Q(receiver__user=request.user)
+        )
+        serializer = TransactionSerializer(queryset, many=True)
+        if queryset:
+            return Response(serializer.data)
+        return Response("No transactions for current user",
+                        status=status.HTTP_404_NOT_FOUND)
+
 
 class TransactionRetrieveDestroyViewSet(
     mixins.RetrieveModelMixin,
@@ -23,23 +35,11 @@ class TransactionRetrieveDestroyViewSet(
     serializer_class = TransactionSerializer
 
 
-# class TransactionListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-#     queryset = Transaction.objects.all()
-#     serializer_class = TransactionSerializer
-#     lookup_fields = ["sender", "receiver"]
-
 class TransactionListAPIView(ListAPIView):
     serializer_class = TransactionSerializer
 
     def get(self, request, *args, **kwargs) -> Response:
-        # sender = self.request.query_params.get('pk')
-        # receiver = self.request.query_params.get('pk')
-        # user = self.request.query_params.get('user')
-        #
-        # queryset = Transaction.objects.filter(
-        #     Q(sender__name=sender) | Q(receiver__name=receiver),
-        #     Q(sender__user=user) | Q(receiver__user=user)
-        # )
+        """Get all transactions where <wallet_name> was sender or receiver"""
 
         queryset = Transaction.objects.filter(
             (
